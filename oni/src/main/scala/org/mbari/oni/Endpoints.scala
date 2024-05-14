954898c2a60fc4fad32d85214ace83a19e163ec0
Brian Schlining <bschlining@gmail.com>
2024-05-14 16:42:14 -0700

    added to main

--- oni/src/main/scala/org/mbari/oni/Endpoints.scala
+++ oni/src/main/scala/org/mbari/oni/Endpoints.scala
@@ -7,7 +7,8 @@
 
 package org.mbari.oni
 
-import org.mbari.oni.endpoints.{AuthorizationEndpoints, HealthEndpoints}
+import jakarta.persistence.EntityManagerFactory
+import org.mbari.oni.endpoints.{AuthorizationEndpoints, ConceptEndpoints, HealthEndpoints, HistoryEndpoints, PhylogenyEndpoints}
 import org.mbari.oni.etc.jwt.JwtService
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.metrics.prometheus.PrometheusMetrics
@@ -19,15 +20,26 @@ object Endpoints:
         val config = AppConfig.DefaultJwtConfig
         JwtService(config.issuer, config.apiKey, config.signingSecret)
 
+    val entityMangerFactory: EntityManagerFactory = AppConfig.DefaultEntityManagerFactory
+
+    val conceptEndpoints: ConceptEndpoints               = ConceptEndpoints(entityMangerFactory)
+    val historyEndpoints: HistoryEndpoints               = HistoryEndpoints(entityMangerFactory)
+    val phylogenyEndpoints: PhylogenyEndpoints           = PhylogenyEndpoints(entityMangerFactory)
+
+
     val authorizationEndpoints: AuthorizationEndpoints = AuthorizationEndpoints()
     val healthEndpoints: HealthEndpoints               = HealthEndpoints()
 
     val prometheusMetrics: PrometheusMetrics[Id] = PrometheusMetrics.default[Id]()
     val metricsEndpoint: ServerEndpoint[Any, Id] = prometheusMetrics.metricsEndpoint
 
-    val endpoints = List(
+
+    val endpoints: List[ServerEndpoint[Any, Id]] = List(
         authorizationEndpoints.allImpl,
-        healthEndpoints.allImpl
+        healthEndpoints.allImpl,
+        conceptEndpoints.allImpl,
+        historyEndpoints.allImpl,
+        phylogenyEndpoints.allImpl
     ).flatten
 
     val allImpl: List[ServerEndpoint[Any, Id]] = endpoints ++ List(metricsEndpoint)
