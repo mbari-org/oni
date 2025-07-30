6e416260fb476bdb283d69f468dcb13bb08e1226
Brian Schlining <bschlining@gmail.com>
2025-07-30 13:05:56 -0700

    Dependency updates

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,14 +15,14 @@ object Dependencies {
     lazy val circeCore               = "io.circe"                      %% "circe-core"                 % circeVersion
     lazy val circeGeneric            = "io.circe"                      %% "circe-generic"              % circeVersion
     lazy val circeParser             = "io.circe"                      %% "circe-parser"               % circeVersion
-    lazy val commonsCodec            = "commons-codec"                  % "commons-codec"              % "1.18.0"
+    lazy val commonsCodec            = "commons-codec"                  % "commons-codec"              % "1.19.0"
     lazy val gson                    = "com.google.code.gson"           % "gson"                       % "2.13.1"
-    val hibernateVersion             = "7.0.6.Final"
+    val hibernateVersion             = "7.0.8.Final"
     lazy val hibernateCore           = "org.hibernate.orm"              % "hibernate-core"             % hibernateVersion
     lazy val hibernateJCache         = "org.hibernate"                  % "hibernate-jcache"           % hibernateVersion
     lazy val hibernateEnvers         = "org.hibernate.orm"              % "hibernate-envers"           % hibernateVersion
     lazy val hibernateHikari         = "org.hibernate.orm"              % "hibernate-hikaricp"         % hibernateVersion
-    lazy val hikariCp                = "com.zaxxer"                     % "HikariCP"                   % "6.3.0"
+    lazy val hikariCp                = "com.zaxxer"                     % "HikariCP"                   % "7.0.0"
     lazy val jansi                   = "org.fusesource.jansi"           % "jansi"                      % "2.4.2"
     lazy val jaspyt                  = "org.jasypt"                     % "jasypt"                     % "1.9.3"
     lazy val junit                   = "junit"                          % "junit"                      % "4.13.2"
@@ -34,7 +34,7 @@ object Dependencies {
     val slf4jVersion                 = "2.0.17"
     lazy val slf4jJulBridge          = "org.slf4j"                      % "jul-to-slf4j"               % slf4jVersion
     lazy val slf4jSystem             = "org.slf4j"                      % "slf4j-jdk-platform-logging" % slf4jVersion
-    private val tapirVersion         = "1.11.37"
+    private val tapirVersion         = "1.11.40"
     lazy val tapirCirce              = "com.softwaremill.sttp.tapir"   %% "tapir-json-circe"           % tapirVersion
     lazy val tapirHelidon            = "com.softwaremill.sttp.tapir"   %% "tapir-nima-server"          % tapirVersion
     lazy val tapirPrometheus         = "com.softwaremill.sttp.tapir"   %% "tapir-prometheus-metrics"   % tapirVersion
